139100520190e987c2bd7fbb46eb8086c1d68e31
Baris G <barisg@pm.me>
2024-01-28 21:13:05 +0100

    Refactor code and fetch game data from API

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import logo from "../assets/logo.svg";
 import search from "../assets/search.svg";
 
 function Navbar() {
@@ -31,19 +31,58 @@ function Navbar() {
       <div className="w-[1200px] mx-auto p-4 flex justify-between">
         <div className="flex items-center gap-4 font-bold">
           <NavLink to="/">
-            <img src={logo} alt="logo of website" />
+            <motion.svg
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              xmlns="http://www.w3.org/2000/svg"
+              initial={{ rotate: 0 }}
+              animate={{ rotate: 360 }}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              whileHover={{ rotate: 0 }}
+              className="fill-current text-secondary hover:text-white transition-colors duration-500"
+            >
+              <path d="M11 14H0V26H11L17 20L11 14ZM8 22H4V18H8V22ZM26 11V0H14V11L20 17L26 11ZM18 4H22V8H18V4ZM14 29V40H26V29L20 23L14 29ZM22 36H18V32H22V36ZM29 14L23 20L29 26H40V14H29ZM36 22H32V18H36V22Z" />
+            </motion.svg>
           </NavLink>
           <div className="bg-white h-8 w-[1px]" />
-          <NavLink to="/" className="relative cursor-pointer">
-            Popular
+          <NavLink
+            to="popular"
+            className="relative cursor-pointer overflow-hidden"
+          >
+            <motion.p
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+            >
+              Popular
+            </motion.p>
             <span className="underline-nav" />
           </NavLink>
-          <NavLink to="/" className="relative cursor-pointer">
-            Last Released
+          <NavLink
+            to="last-released"
+            className="relative cursor-pointer overflow-hidden"
+          >
+            <motion.p
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6, delay: 0.1, ease: "easeOut" }}
+            >
+              Last Released
+            </motion.p>
             <span className="underline-nav" />
           </NavLink>
-          <NavLink to="/" className="relative cursor-pointer">
-            Other Lists
+          <NavLink
+            to="lists"
+            className="relative cursor-pointer overflow-hidden"
+          >
+            <motion.p
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
+            >
+              Other Lists
+            </motion.p>
             <span className="underline-nav" />
           </NavLink>
         </div>
@@ -61,18 +100,28 @@ function Navbar() {
               placeholder="Search for a game"
             />
           </div>
-          <NavLink
-            to="login"
-            className="bg-secondary font-bold py-1 px-4 rounded-3xl border-2 border-transparent hover:border-white transition hover:scale-105"
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            Log In
-          </NavLink>
-          <NavLink
-            to="signup"
-            className="bg-tertiary text-black py-1 px-4 font-bold rounded-3xl border-2 border-transparent hover:border-white transition hover:scale-105"
+            <NavLink
+              to="login"
+              className="bg-secondary font-bold py-[9px] px-4 rounded-3xl"
+            >
+              Log In
+            </NavLink>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            Sign Up
-          </NavLink>
+            <NavLink
+              to="signup"
+              className="bg-tertiary text-black py-[9px] px-4 rounded-3xl font-bold"
+            >
+              Sign Up
+            </NavLink>
+          </motion.div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,45 +1,84 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
 import heroimg from "../assets/Hero.png";
-/*
-initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.3 }}
-*/
+import arrow from "../assets/arrow.svg";
+import arrowb from "../assets/arrowb.svg";
+import GameThumbnail from "../components/GameThumbnail";
+
 function Home() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const url = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=5`;
+
+  const [games, setGames] = useState([]);
+
+  // Fetch de l'API
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data.results);
+        setGames(response.data.results);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
-    <div className="landing h-[70vh] flex items-center">
-      <div className="flex-1">
-        <motion.p
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className="text-7xl font-extrabold text-secondary"
-        >
-          Explore<span className="text-white">,</span> BookMark
-          <span className="text-white">,</span> Share
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className="text-6xl font-extrabold"
-        >
-          The Game You Enjoy
-        </motion.p>
-        <NavLink to="popular">
-          <button
-            type="button"
-            className="p-2 px-4 mt-4 bg-gradient-to-r from-secondary to-blue-500 font-bold rounded-2xl"
+    <>
+      <div className="landing h-screen2 flex items-center">
+        <div className="flex-1">
+          <motion.p
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="text-7xl font-extrabold text-secondary"
           >
-            Get Started
-          </button>
-        </NavLink>
+            Explore<span className="text-white">,</span> BookMark
+            <span className="text-white">,</span> Share
+            <span className="text-white">,</span>
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="text-6xl font-extrabold"
+          >
+            The Game You Enjoy
+          </motion.p>
+          <NavLink to="popular">
+            <button
+              type="button"
+              className="p-2 px-4 mt-4 bg-gradient-to-r from-secondary to-blue-500 font-bold rounded-2xl flex items-center gap-2"
+            >
+              Get Started <img src={arrow} alt="arrow icon" className="w-4" />
+            </button>
+          </NavLink>
+        </div>
+        <div className="flex-1">
+          <img src={heroimg} alt="hero" className="scale-150" />
+        </div>
+        <div className="absolute top-[90%] left-[48%] transform -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-secondary to-blue-500 animate-bounce rounded-full p-1">
+          <img src={arrowb} alt="arrow" className="w-10" />
+        </div>
       </div>
-      <div className="flex-1">
-        <img src={heroimg} alt="hero" className="scale-150" />
+      <div>
+        <div className="flex justify-between items-center">
+          <p className="text-2xl font-bold">Popular among others</p>
+          <NavLink
+            to="popular"
+            className="flex items-center font-bold rounded-2xl p-2 px-4 bg-gradient-to-r from-secondary to-blue-500"
+          >
+            See more
+          </NavLink>
+        </div>
+        <div className="flex my-4 gap-[25px] flex-wrap">
+          {games.map((game) => (
+            <GameThumbnail key={game.id} game={game} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
