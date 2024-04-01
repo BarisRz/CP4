786d0a4d186e6870f22574af6f28b2ef1f9e81c5
Baris G <barisg@pm.me>
2024-04-01 17:34:17 +0200

    Update schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE `played` (
   KEY `userId` (`userId`),
   CONSTRAINT `played_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE,
   CONSTRAINT `played_chk_1` CHECK (((`rating` >= 0) and (`rating` <= 5)))
-) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=147 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `played` (
 --
 
 /*!40000 ALTER TABLE `played` DISABLE KEYS */;
-INSERT INTO `played` VALUES (39,1,4291,0,3),(40,1,3328,0,1.5),(41,1,3498,0,2.5),(75,1,324997,1,5),(79,1,3387,1,5),(80,1,39039,1,5),(81,1,58764,1,5),(82,1,23598,1,4),(83,1,58175,0,4.5),(84,1,22511,0,5),(85,1,10926,1,5),(86,1,58806,0,4),(87,1,537744,0,4),(88,1,4200,1,5),(89,1,12020,0,NULL),(90,1,5679,0,NULL),(91,1,2454,0,NULL),(92,1,1030,0,4),(93,1,13536,1,5),(94,1,28,0,2),(95,1,766,0,2.5),(96,1,5286,0,3.5),(97,1,2551,0,4.5);
+INSERT INTO `played` VALUES (39,1,4291,0,3),(79,1,3387,1,5),(80,1,39039,1,5),(81,1,58764,1,5),(83,1,58175,0,4.5),(84,1,22511,0,5),(85,1,10926,1,4.5),(86,1,58806,0,4),(87,1,537744,0,4),(89,1,12020,0,NULL),(90,1,5679,0,NULL),(91,1,2454,0,NULL),(92,1,1030,0,4),(93,1,13536,1,5),(94,1,28,0,2),(95,1,766,0,2.5),(96,1,5286,0,3.5),(97,1,2551,0,4.5),(115,1,850705,0,5),(118,1,10073,1,5),(119,1,3498,0,NULL),(124,1,4200,1,3),(125,1,56123,1,2),(131,1,3328,0,NULL),(132,21,850705,1,5),(133,21,121,0,3.5),(134,21,742771,1,5),(135,21,960873,0,2),(136,21,374507,1,5),(137,22,326243,1,5),(138,22,4639,0,4),(139,22,324997,1,5),(140,22,10073,0,4.5),(141,22,1571,0,4),(142,22,259801,1,5),(143,23,47137,1,5),(144,23,58501,0,4),(145,23,10142,0,3),(146,23,872778,0,3.5);
 /*!40000 ALTER TABLE `played` ENABLE KEYS */;
 
 --
@@ -59,7 +59,7 @@ CREATE TABLE `utilisateur` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `pseudo` (`pseudo`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,8 +67,9 @@ CREATE TABLE `utilisateur` (
 --
 
 /*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
-INSERT INTO `utilisateur` VALUES (1,'admin','$argon2id$v=19$m=19456,t=2,p=1$SN+0T9MrL9jqgcQFGxkWkQ$umoq98Q/+yuyliBZZ9Wb7mVE0Kdz8TG+x8HXz9pE0IY','admin@admin.com',1);
+INSERT INTO `utilisateur` VALUES (1,'admin','$argon2id$v=19$m=19456,t=2,p=1$SN+0T9MrL9jqgcQFGxkWkQ$umoq98Q/+yuyliBZZ9Wb7mVE0Kdz8TG+x8HXz9pE0IY','admin@admin.com',1),(21,'FightingGameLover','$argon2id$v=19$m=19456,t=2,p=1$deF/OZaOuY9wLZXpn6fKuA$jiNagAeyjRYY/sMjHOWXlMgCKKgiih8NQ0fx3VIAeQY','fighting@gmail.com',0),(22,'RPGLover','$argon2id$v=19$m=19456,t=2,p=1$lI9TImwpioy/zLuNHYpPIw$hHw9zPUegr7X1IcEAph+laGc1gezbTO9xzQBrdAJaBQ','rpg@gmail.com',0),(23,'FortniteKid','$argon2id$v=19$m=19456,t=2,p=1$BxGPlUKYdpAmt48sh2KL3w$7tpudH6ye88vo6RV38P3jvS5saVO7P9m/oWr88ku2MI','fortnite@gmail.com',0);
 /*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+
 --
 -- Dumping routines for database 'cp4'
 --
@@ -82,4 +83,4 @@ INSERT INTO `utilisateur` VALUES (1,'admin','$argon2id$v=19$m=19456,t=2,p=1$SN+0
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-01 19:12:16
+-- Dump completed on 2024-04-01 17:33:32
